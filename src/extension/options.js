551f5111f9e8cc311092f2ecb511ff8df8f6457f
KateZ <hello.katezhang@gmail.com>
2025-06-07 16:56:23 -0400

    feat: Init extension from template and setup the manifest.json
    for more info, pls visit https://github.com/JohnBra/vite-web-extension

--- src/extension/options.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('save').addEventListener('click', () => {
-  const toggle = document.getElementById('toggleFeature').checked;
-  chrome.storage.sync.set({ featureEnabled: toggle }, () => {
-    alert('Settings saved!');
-  });
-});
